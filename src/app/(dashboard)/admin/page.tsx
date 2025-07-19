ed1728583718c9dc67263e51a6f9cbb0cdf4f724
Nenad <nenad@192.168.1.14>
2025-07-19 19:20:34 +0200

    added a few components

---
+++ src/app/(dashboard)/admin/page.tsx
@@ -0,0 +1,23 @@
+import UserCard from "@/components/user-card";
+import React from "react";
+
+
+const page = () => {
+  return (
+    <div className="p-4 flex gap-4 flex-col md:flex-row">
+      {/* LEFT */}
+      <div className="w-full lg:w-2/3">
+      {/* USER CARDS */}
+      <UserCard type="student"/>
+      <UserCard type="teacher"/>
+      <UserCard type="parent"/>
+      <UserCard type="staff"/>
+     
+      </div>
+      {/* RIGHT */}
+      <div className="w-full lg:w-1/3">r</div>
+    </div>
+  );
+};
+
+export default page;
